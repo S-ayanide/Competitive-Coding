b39e28b0e87131b323fd9986b1cb2ce9317c2def
Sayan Mondal <sayanmondal342@gmail.com>
2020-06-19 17:22:57 +0530

    Longest Duplicate

---
+++ June LeetCoding Challenge/Longest Duplicate Substring/Longest Duplicate Substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:   
+    
+    string longestDupSubstring(string S) {
+        string longest;
+        std::unordered_set<std::string_view> set;
+        size_t beg = 1;
+        size_t end = S.size() - 1;
+        while (beg <= end)
+        {
+            auto len = beg + (end - beg) / 2;
+            bool found = false;
+            for (size_t i = 0; i != S.size() - len + 1; ++i)
+            {
+                const auto [it, inserted] = set.emplace(S.data() + i, len);
+                if (!inserted)
+                {
+                    found = true;
+                    longest = std::move(*it);
+                    break;   
+                }
+            }
+            
+            if (found)
+                beg = len + 1;
+            else
+                end = len - 1;
+            
+            set.clear();      
+        }
+            
+        return longest;
+    }
+};
---
+++ June LeetCoding Challenge/Longest Duplicate Substring/README.md
@@ -0,0 +1,26 @@
+# Longest Duplicate Substring
+
+Given a string S, consider all duplicated substrings: (contiguous) substrings of S that occur 2 or more times. (The occurrences may overlap.)
+
+Return any duplicated substring that has the longest possible length. (If S does not have a duplicated substring, the answer is "".)
+
+**Example 1:**
+
+```
+Input: "banana"
+Output: "ana"
+```
+
+**Example 2:**
+
+```
+Input: "abcd"
+Output: ""
+```
+
+> Note:
+
+1. 2 <= S.length <= 10^5
+2. S consists of lowercase English letters.
+
+[Link to Problem](https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/541/week-3-june-15th-june-21st/3365/)
