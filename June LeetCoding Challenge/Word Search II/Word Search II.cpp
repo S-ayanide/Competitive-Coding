2d86500da6b5c108ab20a9999d1ccbbc631c82ad
Sayan Mondal <sayanmondal342@gmail.com>
2020-06-30 23:43:24 +0530

    Word Search Updated

--- June LeetCoding Challenge/Word Search II/Word Search II.cpp
+++ June LeetCoding Challenge/Word Search II/Word Search II.cpp
@@ -1,42 +1,71 @@
+struct TrieNode {
+    string word { "" };
+    array<TrieNode*, 26> chars { nullptr };
+};
+
 class Solution {
 public:
-     bool dfs(vector<vector<char>> &board, int i, int j, string word)
-    {
-        if(word.empty()) return true;
-        int row = board.size();
-        int col = board[0].size();
-        char copy = board[i][j];
-        board[i][j] = '~';
-
-        string str = word.substr(1);
-        if((i-1 >= 0 && board[i-1][j] == word[0] && dfs(board, i-1, j, str)) ||
-           (i+1 < row && board[i+1][j] == word[0] && dfs(board, i+1, j, str)) ||
-           (j-1 >= 0 && board[i][j-1] == word[0] && dfs(board, i, j-1, str)) ||
-           (j+1 < col && board[i][j+1] == word[0] && dfs(board, i, j+1, str))) {
-            board[i][j] = copy;
-            return true;
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        vector<string> result;
+        
+        // insert words to trie
+        TrieNode* root = new TrieNode();
+        for (const auto& word : words) {
+            auto it = root;
+            for (auto c : word) {
+                int idx = c - 'a';
+                if (it->chars[idx] == nullptr) {
+                    it->chars[idx] = new TrieNode();
+                }
+                it = it->chars[idx];
+            }
+            it->word = word;
         }
-        board[i][j] = copy;
-        return false;
+
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board[i].size(); ++j) {
+                int idx = board[i][j] - 'a'; // convert char to idx
+                if (root->chars[idx] != nullptr) {
+                    search(&board, &result, i, j, root);
+                }
+            }
+        }
+
+        return result;
     }
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words)
-    {
-        unordered_set<string> tmp;
-        vector<string> ret;
-        int row = board.size();
-        if(row == 0) return vector<string>();
-        int col = board[0].size();
-        if(col == 0) return vector<string>();
+private:
+    void search(vector<vector<char>>* board_ptr, vector<string>* result_ptr, int i, int j, TrieNode* node) {
+        // dereference pointers (avoiding non-const references)
+		auto& board = *board_ptr;
+        auto& result = *result_ptr;
 
-        for(int k = 0; k < words.size(); k++)
-        for(int i = 0; i < row; i++)
-        for(int j = 0; j < col; j++)
-        {
-            if(board[i][j] == words[k][0] && dfs(board, i, j, words[k].substr(1)))
-                tmp.insert(words[k]);
+        int idx = board[i][j] - 'a'; // convert char to idx
+        node = node->chars[idx];
+        if (node) {
+            if (!empty(node->word)) {
+				// if this is a leaf node (word exists) insert to result and unset it to avoid duplicates
+                result.emplace_back(node->word);
+                node->word = "";
+            }
+
+            // helper function to check for valid direction
+            auto IsValidDir = [&board](int i, int j) -> bool {
+                return i >= 0 && i < board.size()
+                    && j >= 0 && j < board[i].size()
+                    && board[i][j] != '*';
+            };
+
+			// we need to make letters as visited
+            char letter = board[i][j];
+            board[i][j] = '*';
+			
+			// check adjacent cells. we can do this in a loop, but this is faster
+            if (IsValidDir(i - 1, j)) search(board_ptr, result_ptr, i - 1, j, node);
+            if (IsValidDir(i + 1, j)) search(board_ptr, result_ptr, i + 1, j, node);
+            if (IsValidDir(i, j - 1)) search(board_ptr, result_ptr, i, j - 1, node);
+            if (IsValidDir(i, j + 1)) search(board_ptr, result_ptr, i, j + 1, node);
+
+            board[i][j] = letter; // unmark visited
         }
-        for(auto it = tmp.begin(); it != tmp.end(); ++it)
-            ret.push_back(*it);
-        return ret;
     }
 };
